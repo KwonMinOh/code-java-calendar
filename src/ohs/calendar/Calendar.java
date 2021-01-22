9796f639e1b3c96ce185e8b956689833a1c9d40c
권민오 <@DESKTOP-PE5HKJT>
2021-01-22 14:07:56 +0900

    Implement printing calendar (auto ver)

--- src/ohs/calendar/Calendar.java
+++ src/ohs/calendar/Calendar.java
@@ -1,57 +1,91 @@
 package ohs.calendar;
 
-
-
 public class Calendar {
 
-	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31,
-			30, 31 };
-	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31,
-		30, 31 };
+	private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30,
+			31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31,
+			31, 30, 31, 30, 31 };
 
-	public boolean isLeapYear(int year){
-		if(year % 4 == 0 &&  (year  % 100 != 0 || year % 400 != 0))
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 			return true;
 		else
 			return false;
 	}
-	
+
 	public int getmaxDaysOfMonth(int year, int month) {
-			if(isLeapYear(year)){
-				return LEAP_MAX_DAYS[month - 1];
-			}else{
-				return MAX_DAYS[month - 1];
-			}
-	
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month];
+		} else {
+			return MAX_DAYS[month];
+		}
+
 	}
 
-	public void printCalender(int year,int month, int weekday) {
-		System.out.printf("   <<%4d %3d월>>\n", year,  month);
+	public void printCalender(int year, int month) {
+		System.out.printf("   <<%4d %3d월>>\n", year, month);
 		System.out.println("   일    월    화    수    목    금    토");
 		System.out.println("---------------------------");
-			for (int i = 0; i < weekday; i++){
-				System.out.print("   ");
-			}
-			int maxDay = getmaxDaysOfMonth(year, month);
-			int count = 7 - weekday;
-			int delim = (count < 7) ? count : 0;
-			
-			
-			for (int i = 1; i <= count; i++){
-				System.out.printf("%3d",i);
-			}
-			System.out.println();
-			
-			count++;
-			for(int i = count; i <= maxDay; i++){
-			System.out.printf("%3d",i);
-			if(i % 7 == delim)
-					System.out.println();
+
+		int weekday = getWeekDay(year, month, 1);
+
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+		int maxDay = getmaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
+
+		for (int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
+
+		count++;
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim)
+				System.out.println();
 		}
 		System.out.println();
 		System.out.println();
-	
+
 	}
 
+	private int getWeekDay(int year, int month, int day) {
+		int syear = 1970;
+		final int STANDARDWEEKDAY= 3;//1970/Jan/Thursday
+		
+		int count = 0;
+		
+		for(int i = syear; i < year;i++){
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;	
+		}
+		
+		//System.out.println(count);
+		for(int i = 1; i < month ; i++){
+			int delta = getmaxDaysOfMonth(year, i);
+			count += delta;
+		}
+		
+		count += day;
+
+		int weekday = (count + STANDARDWEEKDAY) % 7;
+		return weekday;
+	}
 	
+	
+	public static void main(String[] args) {
+		Calendar cal = new Calendar();
+		System.out.println(cal.getWeekDay(1970,1,1) == 3);
+		System.out.println(cal.getWeekDay(1971,1,1) == 4);
+		System.out.println(cal.getWeekDay(1972,1,1) == 5);
+		System.out.println(cal.getWeekDay(1973,1,1) == 0);
+		System.out.println(cal.getWeekDay(1974,1,1) == 1);
+
+
+
+	}
 }
--- src/ohs/calendar/Prompt.java
+++ src/ohs/calendar/Prompt.java
@@ -3,69 +3,65 @@ package ohs.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	
-		public int parseDay(String week){
-			if(week.equals("su"))return 0;
-			else if(week.equals("mo"))return 1;
-			else if(week.equals("tu"))return 2;
-			else if(week.equals("wd"))return 3;
-			else if(week.equals("th"))return 4;
-			else if(week.equals("fr"))return 5;
-			else if(week.equals("sa"))return 6;
-			else
-				return 0;
-		}
-		
-	
-		public void runPrompt(){
-			// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-			Scanner scanner = new Scanner(System.in);
-			Calendar cal = new Calendar();
-			
 
-			
-			
-			int month = -1;
-			int year = -1;
-			int weekday = 0;
-			
-			while(true){
-				System.out.println("년도를 입력하세요.(exit : -1)");
-				System.out.println("Year>");
-				year = scanner.nextInt();
-				if(year == -1)
-					break;
-				
-				System.out.println("달을 입력하세요.");
-				System.out.println("Month>");
-				month = scanner.nextInt();
-				System.out.println("첫째날의 요일을 입력하세요(su, mo, tu, wd, th, fr, sa).");
-				String str_weekday = scanner.next();
-				weekday =  parseDay(str_weekday);
-				
-				if(month > 12 || month < 1){
-					System.out.println("잘못된입력입니다.");
-					continue;
-				}
-				cal.printCalender(year, month, weekday);
+	public int parseDay(String week) {
+		if (week.equals("su"))
+			return 0;
+		else if (week.equals("mo"))
+			return 1;
+		else if (week.equals("tu"))
+			return 2;
+		else if (week.equals("wd"))
+			return 3;
+		else if (week.equals("th"))
+			return 4;
+		else if (week.equals("fr"))
+			return 5;
+		else if (week.equals("sa"))
+			return 6;
+		else
+			return 0;
+	}
+
+	public void runPrompt() {
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		int month = -1;
+		int year = -1;
+
+		while (true) {
+			System.out.println("년도를 입력하세요.(exit : -1)");
+			System.out.println("Year>");
+			year = scanner.nextInt();
+			if (year == -1)
+				break;
+
+			System.out.println("달을 입력하세요.");
+			System.out.println("Month>");
+			month = scanner.nextInt();
+
+			if (month > 12 || month < 1) {
+				System.out.println("잘못된입력입니다.");
+				continue;
 			}
-			
-			System.out.println("bye~");
-			
-			//cal.printSampleCalender();
-			
-			scanner.close();
+			cal.printCalender(year, month);
 		}
 
-		
+		System.out.println("bye~");
 
-	
-		// TODO Auto-generated method stub
-		public static void main(String[] args) {
-			
-			//셀 실행
-			Prompt p = new Prompt();
-			p.runPrompt();
+		// cal.printSampleCalender();
+
+		scanner.close();
+	}
+
+	// TODO Auto-generated method stub
+	public static void main(String[] args) {
+
+		// 셀 실행
+		Prompt p = new Prompt();
+		p.runPrompt();
 	}
 
 }
